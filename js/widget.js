e1f3fc02f291983ac04c18a3c59084825aaaa2ab
Taras Chuiko <taraschuiko@gmail.com>
2017-11-19 13:13:48 +0200

    v.1

--- js/widget.js
+++ js/widget.js
@@ -1,9 +1,26 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4) {
-		document.getElementById('employeeList').innerHTML = xhr.responseText;
+jQuery(document).ready(function($) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			var array = JSON.parse(xhr.responseText);
+			// console.log(typeof(array));
+			var genHTML = "";
+			for (var i = 0; i < array.length; i++) {
+				var office;
+				if (array[i].inoffice) {
+					office = "in";
+				} else {
+					office = "out";
+				}
+				genHTML += "<div class='workers " + office + "'>" + "<p class='available'>" + office + "</p><p>" + array[i].name + "</p></div>";
+			}
+			console.log(genHTML);
+			$('#employeeList').html(genHTML);
+		}
 	}
-}
 
-xhr.open('GET', '../data/employees.json');
-xhr.send();
+	xhr.open('GET', '../data/employees.json');
+	xhr.send();
+
+	
+});
